0891a31da28a8a5549164d2e7973924db379cdce
Chamod Gihan Pathirana <gihanpatgirana84@gmail.com>
2023-12-09 11:16:33 +0530

    login page

--- app/src/main/java/com/example/studentmarkscalculater/calculater.kt
+++ app/src/main/java/com/example/studentmarkscalculater/calculater.kt
@@ -2,10 +2,26 @@ package com.example.studentmarkscalculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class calculater : AppCompatActivity() {
+
+    val mainfragment = MainFragment()
+    val calfragment = calculater()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculater)
+
+        val btn2 = findViewById<Button>(R.id.button2)
+        val btn3 = findViewById<Button>(R.id.button3)
+
+        btn2.setOnClickListener {
+
+        }
+
+        btn3.setOnClickListener {
+
+        }
+
     }
 }
